b3e2badcf2c5bef4899853429742a7364b184faa
Martien Dermawan Tanama <martiendt@gmail.com>
2026-09-14 11:20:40 +0700

    x

--- src/router/index.ts
+++ src/router/index.ts
@@ -122,7 +122,7 @@ router.beforeEach(async (to, from) => {
 
   // try to reauthenticate if not authenticated
   if (!authStore.isAuthenticated && to.path !== '/signin') {
-    // await authStore.reauthenticate().catch(() => {});
+    await authStore.reauthenticate().catch(() => {});
   }
 
   // check if user has permission to access route
@@ -142,9 +142,9 @@ router.beforeEach(async (to, from) => {
   }
 
   // redirect to signin page if not authenticated
-  if (to.meta.requiresAuth && !authStore.isAuthenticated) {
-    // return `/admin/signin?redirect=${encodeURIComponent(to.fullPath)}`;
-  }
+  return `/admin/signin?redirect=${encodeURIComponent(to.fullPath)}`;
+  // if (to.meta.requiresAuth && !authStore.isAuthenticated) {
+  // }
 });
 
 export default router;
